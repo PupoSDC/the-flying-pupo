06ca6838d7d42cb766b8a32d19f7c51184eeff81
PupoSDC <pedro.pupo@greator.com>
2020-10-23 20:06:42 +0200

    Make mobile friendly

--- src/views/MapView.tsx
+++ src/views/MapView.tsx
@@ -1,17 +1,25 @@
 import React, { FunctionComponent, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { LatLngLiteral } from "leaflet";
-import { Map, Polyline, TileLayer } from "react-leaflet";
-import { List, ListItem, ListItemText } from "@material-ui/core";
-import { blue, red } from "@material-ui/core/colors";
+import { Map, TileLayer } from "react-leaflet";
+import {
+  Fab,
+  Fade,
+  List,
+  ListItem,
+  ListItemText,
+  SwipeableDrawer,
+} from "@material-ui/core";
+import EditIcon from "@material-ui/icons/Edit";
 import flights from "../assets/flights";
 import FlyingIcon from "../components/leaflet/FlyingIcon";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   container: {
     display: "flex",
     flexDirection: "row",
     height: "100%",
+    position: "relative",
   },
   menu: {
     width: 250,
@@ -22,10 +30,17 @@ const useStyles = makeStyles({
     height: "100%",
     flex: 1,
   },
-});
+  fab: {
+    position: "absolute",
+    bottom: theme.spacing(1),
+    left: theme.spacing(1),
+    zIndex: 1000,
+  },
+}));
 
 const MapView: FunctionComponent<{}> = () => {
   const styles = useStyles();
+  const [drawerOpen, setDrawerOpen] = useState(false);
   const [selected, setSelected] = useState<string[]>([]);
 
   const { latitude, longitude } = flights[
@@ -37,40 +52,59 @@ const MapView: FunctionComponent<{}> = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <List className={styles.menu}>
-        {flights
-          .slice()
-          .reverse()
-          .map(({ identification: { name, description, id } }) => (
-            <ListItem
-              button
-              key={id}
-              onClick={() =>
-                setSelected((selected) =>
-                  selected.includes(id)
-                    ? selected.filter((s) => s !== id)
-                    : [...selected, id]
-                )
-              }
-              selected={selected.includes(id)}
-            >
-              <ListItemText primary={name} secondary={description} />
-            </ListItem>
-          ))}
-      </List>
-      <Map center={position} zoom={11} className={styles.map}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        />
-        {flights
-          .filter(({ identification: { id } }) => selected.includes(id))
-          .map((flight) => (
-            <FlyingIcon key={flight.identification.id} track={flight.track} />
-          ))}
-      </Map>
-    </div>
+    <>
+      <div className={styles.container}>
+        <SwipeableDrawer
+          anchor="left"
+          open={drawerOpen}
+          onOpen={() => setDrawerOpen(true)}
+          onClose={() => setDrawerOpen(false)}
+        >
+          <List className={styles.menu}>
+            {flights
+              .slice()
+              .reverse()
+              .map(({ identification: { name, description, id } }) => (
+                <ListItem
+                  button
+                  key={id}
+                  selected={selected.includes(id)}
+                  onClick={() =>
+                    setSelected((selected) =>
+                      selected.includes(id)
+                        ? selected.filter((s) => s !== id)
+                        : [...selected, id]
+                    )
+                  }
+                >
+                  <ListItemText primary={name} secondary={description} />
+                </ListItem>
+              ))}
+          </List>
+        </SwipeableDrawer>
+        <Map center={position} zoom={11} className={styles.map}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          />
+          {flights
+            .filter(({ identification: { id } }) => selected.includes(id))
+            .map((flight) => (
+              <FlyingIcon key={flight.identification.id} track={flight.track} />
+            ))}
+        </Map>
+        <Fade in={!drawerOpen}>
+          <Fab
+            onClick={() => setDrawerOpen(true)}
+            className={styles.fab}
+            aria-label="edit"
+            size="small"
+          >
+            <EditIcon />
+          </Fab>
+        </Fade>
+      </div>
+    </>
   );
 };
 
