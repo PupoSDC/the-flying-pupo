77426350353aecc5e7539168cefc5fecdf19b575
PupoSDC <pedro.pupo@greator.com>
2021-03-28 20:10:02 +0200

    Re-organize folder structure

---
+++ src/assets/flights/2021/index.ts
@@ -0,0 +1,14 @@
+export const flights2021 = [
+  require("./20210116EDKBEDKB").flight,
+  require("./20210221EDKBEDGS").flight,
+  require("./20210221EDGSEDKB").flight,
+  require("./20210305EDKBEDGS").flight,
+  require("./20210305EDGSEDGS").flight,
+  require("./20210305EDGSEDKB").flight,
+  require("./20210306EDKBEDRT").flight,
+  require("./20210306EDRTEDRT").flight,
+  require("./20210306EDRTEDKB").flight,
+  require("./20210307EDKBEDKB").flight,
+  require("./20210320EDKBEDKB").flight,
+  require("./20210328EDKBEDKB").flight,
+];
