6ed259ff508e442f7b4c9a381ba7be58ee572d34
Pedro Pupo Sa da Costa <pedro@hublo.com>
2022-02-19 21:20:16 +0100

    feast: add 300NM flights

--- src/content/flights/2022/index.ts
+++ src/content/flights/2022/index.ts
@@ -5,4 +5,8 @@ export const flights2022 = [
   require("./20220205EDRKEDKB").flight,
   require("./20220213EDKBEDKB").flight,
   require("./20220214EDKBEDKB").flight,
+
+  require("./20220219EDKBEDSB").flight,
+  require("./20220219EDSBEDRW").flight,
+  require("./20220219EDRWEDKB").flight,
 ] as Flight[];
