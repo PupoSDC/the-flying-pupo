7c31ef6c32bbe85ad3ceb18bee5aec69a9724df7
Pedro Pupo Sa da Costa <pedro@hublo.com>
2022-11-02 23:37:08 +0100

    feat: add flight 20221102

--- records/flights/2022/index.ts
+++ records/flights/2022/index.ts
@@ -58,4 +58,5 @@ export const flights2022 = [
   require("./20221025EPLLEPLL").flight,
   require("./20221028EPLLEPLL").flight,
   require("./20221028EPLLEPLL2").flight,
+  require("./20221102EPLLEPLL").flight
 ] as Flight[];
