a602be41caa726695bd5a61198e0ee210d3d3e0f
Pedro Pupo Sa da Costa <pedro@hublo.com>
2023-01-08 16:02:36 +0100

    feat: force dark theme always

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from "react";
 import "./global.css";
 
 const RootLayout: FunctionComponent = ({ children }) => (
-  <html>
+  <html data-theme="dark">
     <head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </head>
